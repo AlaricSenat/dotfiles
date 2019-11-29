ef7c82d6efddb2b071b5ca6aa97a679891ebf593
Alaric Senat <asenat@localhost.localdomain>
2019-11-29 15:59:34 +0100

    Typo fix

--- nvim/install.sh
+++ nvim/install.sh
@@ -3,6 +3,6 @@
 cp -rf vim "${HOME}"/.vim
 mkdir -p "${HOME}"/.vim/swap
 mkdir -p "${HOME}"/.vim/undodir
-mkdir -p:x
+mkdir -p "${HOME}"/.config/nvim
 cp -f coc-settings.json coc.vim init.vim nvimrc.vim "${HOME}"/.config/nvim
 nvim +PlugInstall +q +q
