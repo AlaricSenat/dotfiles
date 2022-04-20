8a62cab4903b41ad2bcb204cee4056f79e535a4b
Alaric Senat <dev.asenat@posteo.net>
2022-04-20 16:37:54 +0200

    nvim: Add forgotten vlc.vim

--- nvim/install.sh
+++ nvim/install.sh
@@ -4,5 +4,5 @@ cp -rf vim "${HOME}"/.vim
 mkdir -p "${HOME}"/.vim/swap
 mkdir -p "${HOME}"/.vim/undodir
 mkdir -p "${HOME}"/.config/nvim
-cp -f coc-settings.json coc.vim init.vim nvimrc.vim "${HOME}"/.config/nvim
+cp -f coc-settings.json coc.vim init.vim nvimrc.vim vlc.vim "${HOME}"/.config/nvim
 nvim +PlugInstall +q +q
