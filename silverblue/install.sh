994c45f9005f69bc1788d55ae261dac5974e6372
Alaric Senat <dev.asenat@posteo.net>
2022-04-29 14:45:03 +0200

    silverblue: Remove now unused `Alacritty` package

--- silverblue/install.sh
+++ silverblue/install.sh
@@ -2,10 +2,6 @@ FVERSION=$(rpm -E %fedora)
 # Enable rpmfusion repositories
 sudo rpm-ostree install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$FVERSION.noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$FVERSION.noarch.rpm
 
-# Enable Alacritty's repository
-
-#sudo wget https://copr.fedorainfracloud.org/coprs/pschyska/alacritty/repo/fedora-$FVERSION/pschyska-alacritty-fedora-$FVERSION.repo -O /etc/yum.repos.d/pschyska-alacritty-fedora-$FVERSION.repo
-
 # Enable flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
