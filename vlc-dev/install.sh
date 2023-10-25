e3a034acb555ed298ffa7b27fa76017078598930
Alaric Senat <dev.asenat@posteo.net>
2023-10-25 14:56:53 +0200

    vlc-devel: extend required packages

--- vlc-dev/install.sh
+++ vlc-dev/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo dnf builddep -y vlc vlc-devel
-sudo dnf install -y libasan libtsan libubsan qt5-qtquickcontrols2-devel libatomic
+sudo dnf install -y libasan libtsan libubsan qt5-qtquickcontrols2-devel libatomic lame-devel valgrind-devel
 
 WORKSPACE="/var/home/asenat/workspaces/vlc"
 VLC="${WORKSPACE}/vlc"
