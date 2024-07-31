8a465c5f9d567150f23559d705c9698449a603a2
Alaric Senat <dev.asenat@posteo.net>
2024-07-31 11:19:46 +0200

    silverblue: layer useful dev packages

--- silverblue/phase2.sh
+++ silverblue/phase2.sh
@@ -1,8 +1,19 @@
 # Install the missing packages from rpmfusion
-rpm-ostree install ffmpeg &&
+rpm-ostree install                                            \
+    android-tools                                             \
+    git                                                       \
+    podman-compose containernetworking-plugins                \
+    virt-manager                                              \
+    intel-media-driver libva-intel-media-driver igt-gpu-tools \
+    net-tools tcpdump wireshark                               \
+    chromium &&
 
 # Install some everyday-use flatpaks
 
-flatpak install -y flathub com.spotify.Client org.videolan.VLC org.gnome.Geary &&
+flatpak install -y flathub                                    \
+com.spotify.Client                                            \
+org.videolan.VLC                                              \
+org.gnome.Geary                                               \
+org.mozilla.firefox &&
 
 echo Success ! Reboot and the fully layered silverblue install is done
