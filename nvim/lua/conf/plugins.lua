7575b888a87dd261c30954c86de94c78d04484f3
Alaric Senat <dev.asenat@posteo.net>
2024-07-31 11:21:10 +0200

    nvim: bump telescope

--- nvim/lua/conf/plugins.lua
+++ nvim/lua/conf/plugins.lua
@@ -42,7 +42,7 @@ return packer.startup(function(use)
 
     -- Fuzzy finder
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
         requires = {
             { 'nvim-lua/plenary.nvim' },
             { 'nvim-telescope/telescope-fzf-native.nvim',
