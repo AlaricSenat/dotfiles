97e75671e6f849b411bb7c255bca3553fb796dbf
Alaric Senat <alaric@videolabs.io>
2024-08-19 11:31:15 +0200

    nvim: codespell: add an ignore word list

--- nvim/lua/conf/lsp.lua
+++ nvim/lua/conf/lsp.lua
@@ -87,7 +87,9 @@ local null_ls = require('null-ls')
 null_ls.setup({
     sources = {
         null_ls.builtins.diagnostics.markdownlint,
-        null_ls.builtins.diagnostics.codespell,
+        null_ls.builtins.diagnostics.codespell.with({
+            extra_args = {'-L', 'crate,IIF'},
+        }),
 
         null_ls.builtins.formatting.beautysh,
     },
