1093b8896b967577a34f288f000b9deac464eb9a
Alaric Senat <alaric@videolabs.io>
2025-02-25 14:12:03 +0100

    nvim: telescope: add lsp diagnostic keymap

--- nvim/lua/conf/telescope.lua
+++ nvim/lua/conf/telescope.lua
@@ -25,5 +25,6 @@ vim.keymap.set('n', '<C-f>', builtin.find_files, {})
 vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
 vim.keymap.set('n', '<C-b>', builtin.buffers, {})
 vim.keymap.set('n', '<C-h>', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>dd', builtin.diagnostics, {})
 
 telescope.load_extension('fzf')
