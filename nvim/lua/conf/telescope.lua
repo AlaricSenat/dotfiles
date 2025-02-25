852827d4be6a28f8eff730a0d231646d69041250
Alaric Senat <alaric@videolabs.io>
2025-02-25 14:12:23 +0100

    nvim: telescope: add git status keymap

--- nvim/lua/conf/telescope.lua
+++ nvim/lua/conf/telescope.lua
@@ -26,5 +26,6 @@ vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
 vim.keymap.set('n', '<C-b>', builtin.buffers, {})
 vim.keymap.set('n', '<C-h>', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>dd', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>hs', builtin.git_status, {})
 
 telescope.load_extension('fzf')
